258a1ddac26d6c150a848363cc3355be64b29276
mogamoga <mogamoga.g1thu6@gmail.com>
2022-01-02 23:42:56 +0900

    a

--- js/main.js
+++ js/main.js
@@ -1,23 +1,31 @@
 
-const text = "んにゃぴ";
+const charArray = [
+    new に(),
+    new ん(),
+    new じ(),
+    new ん()
+];
 
-const charKeyDic = {
-    "ん": {
-        romanList: [
-            ["n", "n"],
-        ],
-        checkRoman: function() {
-            
-        }
-    },
-    "に": {},
-    "にゃ": {},
-    "ゃ": {},
-    "ぴ": {}
-};
+charArray[0].nextChar = charArray[1];
+charArray[1].nextChar = charArray[2];
+charArray[2].nextChar = charArray[3];
+
+let currentIndex = 0;
 
 $(window).keydown(function(e) {
-    console.log(e.key, e.shiftKey);
+    if (currentIndex >= charArray.length) return;
+
+    const result = charArray[currentIndex].checkRoman(e.key);
+    if (result === ROMAN_NG) {
+        console.log("違うよん", e.key);
+    }
+    else if (result === ROMAN_KEEP) {
+        console.log("KEEP", e.key);
+    }
+    else if (result === ROMAN_OK) {
+        console.log("OK", e.key);
+        currentIndex += 1;
+    }
 });
 
 
--- js/roman.js
+++ js/roman.js
@@ -1,103 +1,104 @@
 
-const ROMAN_NG = 0;
-const ROMAN_KEEP = 1;
-const ROMAN_OK = 2;
-
 function あ() {
-    this.nextChar = null;
-    this._inputRoman = [];
-    this._expectRomanArray = [["a"]];
+    Char.call(this);
+    this.expectRomanArray = [
+        ["a"]
+    ];
 }
 
-あ.prototype.checkRoman = function(roman) {
-    if (roman === this._expectRomanArray[0][0]) {
-        return ROMAN_OK;
-    }
-    return ROMAN_NG;
-};
+あ.prototype = Object.create(Char.prototype);
+あ.prototype.constructor = あ;
 
 function じ() {
-    this.nextChar = null;
-    this._expectRomanArray = [
+    Char.call(this);
+    this.expectRomanArray = [
         ["j", "i"], ["z", "i"],
     ];
-    this._nextExpectRomanIndex = 0;
-    this._expectRoman = null;
 }
 
-じ.prototype.checkRoman = function(roman) {
-    if (this._nextExpectRomanIndex === 0) {
-        for (let i = 0; i < this._expectRomanArray.length; i++) {
-            if (roman === this._expectRomanArray[i][0]) {
-                this._nextExpectRomanIndex += 1;
-                this._expectRoman = this._expectRomanArray[i];
-                if (this._nextExpectRomanIndex < this._expectRoman.length) {
-                    return ROMAN_KEEP;
-                }
-                else {
-                    return ROMAN_OK;
-                }
-            }
-        }
-    }
-    else {
-        if (roman === this._expectRoman[this._nextExpectRomanIndex]) {
-            this._nextExpectRomanIndex += 1;
-            if (this._nextExpectRomanIndex < this._expectRoman.length) {
-                return ROMAN_KEEP;
-            }
-            else {
-                return ROMAN_OK;
-            }
-        }
-        else {
-            return ROMAN_NG;
-        }
-    }
-};
+じ.prototype = Object.create(Char.prototype);
+じ.prototype.constructor = じ;
 
 function に() {
-    this.nextChar = null;
-    this._inputRoman = [];
-    this._expectRomanArray = [["n", "i"]];
+    Char.call(this);
+    this.expectRomanArray = [
+        ["n", "i"]
+    ];
 }
 
+に.prototype = Object.create(Char.prototype);
+に.prototype.constructor = に;
+
 に.prototype.checkRoman = function(roman) {
-    if (roman === this._expectRomanArray[0][0]) {
-        this._inputRoman.push(roman);
-        return ROMAN_KEEP;
-    }
-    else {
-        if (roman === this._expectRomanArray[0][1]) {
-            return ROMAN_OK;
+    const result = Char.prototype.checkRoman(roman);
+    if (result === ROMAN_NG) {
+        if (this.nextChar !== null) {
+            // todo
         }
         else {
             return ROMAN_NG;
         }
     }
-};
-
-function ん() {
-    this.nextChar = null;
-    this._inputRoman = [];
-    this._expectRomanArray = [["n", "n"]];
 }
 
+function にゃ() {
+    Char.call(this);
+    this.expectRomanArray = [
+        ["n", "y", "a"]
+    ];
+}
+
+にゃ.prototype = Object.create(Char.prototype);
+にゃ.prototype.constructor = にゃ;
+
+function ぴ() {
+    Char.call(this);
+    this.expectRomanArray = [
+        ["p", "i"]
+    ];
+}
+
+ぴ.prototype = Object.create(Char.prototype);
+ぴ.prototype.constructor = ぴ;
+
+function ゃ() {
+    Char.call(this);
+    this.expectRomanArray = [
+        ["x", "y", "a"]
+    ];
+}
+
+ゃ.prototype = Object.create(Char.prototype);
+ゃ.prototype.constructor = ゃ;
+
+function ん() {
+    Char.call(this);
+    this.expectRomanArray = [
+        ["n", "n"]
+    ];
+    this.expectRoman = this.expectRomanArray[0];
+}
+
+ん.prototype = Object.create(Char.prototype);
+ん.prototype.constructor = ん;
+
 ん.prototype.checkRoman = function(roman) {
-    if (this._inputRoman.length === 0) {
-        if (roman === this._expectRomanArray[0][0]) {
-            this._inputRoman.push(roman);
+    if (this.nextExpectRomanIndex === 0) {
+        if (roman === this.expectRoman[0]) {
+            this.nextExpectRomanIndex += 1;
             return ROMAN_KEEP;
         }
     }
     else {
-        if (roman === this._expectRomanArray[0][1]) {
+        if (roman === this.expectRoman[1]) {
+            this.nextExpectRomanIndex += 1;
             return ROMAN_OK;
         }
         else if (this.nextChar == null) {
             return ROMAN_NG;
         }
         else if (this.nextChar.checkRoman(roman) !== ROMAN_NG) {
+            this.nextExpectRomanIndex += 1;
             return ROMAN_OK;
         }
         else {
