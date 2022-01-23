7e4272e6694502390b3e23aea0ce259c96c915ce
mogamoga <mogamoga.g1thu6@gmail.com>
2022-01-23 21:48:37 +0900

    おーええやん

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,8 @@
 
+const GAME_START = 0;
+const GAME_PLAY = 1;
+const GAME_CLEAR = 2;
+
 const CHAR_NG = 0;
 const CHAR_KEEP = 1;
 const CHAR_COMPLETE = 2;
--- js/typingGame.js
+++ js/typingGame.js
@@ -3,19 +3,43 @@ function TypingGame() {
     this.okCount = 0;
     this.ngCount = 0;
     this.textIndex = 0;
-    this.currentText = new Text(hiraganaTextArray[this.textIndex]);
-
+    this.status = GAME_START;
+    this.content = null;
+    this.currentText = null;
     this.$typingText = $("#typing-text");
     this.$typedRoman = $("#typed");
     this.$notTypedRoman = $("#not-typed");
     this.$okCount = $("#ok-count");
     this.$ngCount = $("#ng-count");
 
-    this.$typingText.text(textArray[this.textIndex]);
+    this.$typingText.text("タイピングゲーム");
+    this.$notTypedRoman.text("F1, F2, F3キーを押すとゲームが始まります。");
+    this.$okCount.text("0");
+    this.$ngCount.text("0");
+};
+
+TypingGame.prototype.gameStart = function(index) {
+    this.okCount = 0;
+    this.ngCount = 0;
+    this.textIndex = 0;
+    this.status = GAME_PLAY;
+    this.content = TypingContents.select(index);
+    this.currentText = new Text(this.content.hiraganaTextArray[this.textIndex]);
+    
+    this.$typingText.text(this.content.textArray[this.textIndex]);
+    this.$okCount.text("0");
+    this.$ngCount.text("0");
     this.setupExpectRoman();
 };
 
-TypingGame.prototype.inputRoman = function(roman) {
+TypingGame.prototype.gameClear = function() {
+    this.status = GAME_CLEAR;
+    this.$typingText.text("Game Clear");
+    this.$typedRoman.text("");
+    this.$notTypedRoman.text("F1, F2, F3キーを押すとゲームが始まります。");
+};
+
+TypingGame.prototype.gamePlay = function(roman) {
     if (TypingManager.validRoman(roman) === false) return;
 
     switch (this.currentText.inputRoman(roman)) {
@@ -32,13 +56,32 @@ TypingGame.prototype.inputRoman = function(roman) {
             console.log(roman, "OK");
             this.$okCount.text(++this.okCount);
             this.updateExpectRoman(roman);
-            if (this.textIndex < hiraganaTextArray.length - 1) {
-                this.currentText = new Text(hiraganaTextArray[++this.textIndex]);
-                this.$typingText.text(textArray[this.textIndex]);
+            if (this.textIndex < this.content.hiraganaTextArray.length - 1) {
+                this.currentText = new Text(this.content.hiraganaTextArray[++this.textIndex]);
+                this.$typingText.text(this.content.textArray[this.textIndex]);
                 this.setupExpectRoman();
+            }
+            else {
+                this.gameClear();
             }
             break;
     }
+};
+
+TypingGame.prototype.keydown = function(e) {
+    const key = e.key;
+
+    switch (key) {
+        case "F1": this.gameStart(0); return false;
+        case "F2": this.gameStart(1); return false;
+        case "F3": this.gameStart(2); return false;
+    }
+
+    if (this.status === GAME_PLAY) {
+        this.gamePlay(key);
+    }
+
+    return true;
 };
 
 TypingGame.prototype.setupExpectRoman = function() {
